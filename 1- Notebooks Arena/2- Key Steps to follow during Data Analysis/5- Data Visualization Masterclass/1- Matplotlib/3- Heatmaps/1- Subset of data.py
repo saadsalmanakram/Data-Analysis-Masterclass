b913725fde975c0efcb9c37dd325855ebdc07d63
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-11 22:02:33 +0500

    updates

--- 1- Notebooks Arena/2- Key Steps to follow during Data Analysis/5- Data Visualization Masterclass/1- Matplotlib/3- Heatmaps/1- Subset of data.py
+++
@@ -1,7 +0,0 @@
-# Generate a heatmap for a selected subset of the data
-plt.imshow(df[['column1', 'column2', 'column3']].corr(), cmap='viridis', interpolation='none')
-plt.colorbar()
-plt.xticks(np.arange(3), ['Column1', 'Column2', 'Column3'], rotation=90)
-plt.yticks(np.arange(3), ['Column1', 'Column2', 'Column3'])
-plt.title('Heatmap of Selected Columns')
-plt.show()
