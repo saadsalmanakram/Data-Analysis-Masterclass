dca92e7074f38696f51beabaf6470e3febc0bbb7
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-11 22:10:51 +0500

    updates

--- 1- Notebooks Arena/2- Key Steps to follow during Data Analysis/5- Data Visualization Masterclass/1- Matplotlib/1- Plots based on kinds of Data/1- Histograms/1- Single variable.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-# Plot a histogram to understand the distribution of a variable
-plt.hist(df['column_name'], bins=20, edgecolor='black')
-plt.title('Histogram of Column Name')
-plt.xlabel('Value')
-plt.ylabel('Frequency')
-plt.show()
